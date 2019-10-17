1db2090876cdb6a0a66dd05e5649832e628182c0
EdwinBoersema <edwinboersema@gmail.com>
2019-10-17 11:04:48 +0200

    connected the input values to the buttons.

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
+// declaring global variables
 var wordToGuess = "woord";
+var letterGuess = "";
+var wordGuess = "";
 
+//adding listeners to the buttons and running functions when they're being clicked.
 var guessLetterButton = document.getElementById("guessLetterButton");
 guessLetterButton.addEventListener('click', function(){
-    console.log("connected guess letter button");
+    let letterInput = document.getElementById("guessLetterInput").value;
+    console.log(letterInput);
+    document.getElementById("guessLetterInput").value = "";
 });
 
 var guessWordButton = document.getElementById("guessWordButton");
 guessWordButton.addEventListener('click', function(){
-    console.log("connected guess word button");
+    let wordInput = document.getElementById("guessWordInput").value;
+    console.log(wordInput);
+    document.getElementById("guessWordInput").value = "";
 });
 
 var newWordButton = document.getElementById("newWordButton");
 newWordButton.addEventListener('click', function(){
-    console.log("connected new word button");
+    let newWord = document.getElementById("newWordInput").value;
+    console.log(newWord);
+    document.getElementById("newWordInput").value = "";
 });
 
 document.getElementsByTagName("button")[0].addEventListener('click', function () {
